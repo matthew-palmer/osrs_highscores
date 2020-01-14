2157c8a1f0879b42f95fc3abaf5db2ff5fb4950a
Matt Palmer <matt.palmer@thermofisher.com>
2020-01-13 22:26:29 -0500

    [enhancement_rankings] Fix to #4 for paging issue on edge ranks.

--- osrs_highscores/rankings.py
+++ osrs_highscores/rankings.py
@@ -38,7 +38,7 @@ class Rankings(OSRSBase):
         time.sleep(0.1)
 
         table = OSRSInfo().index_inverse[skill]
-        rank_page = int(float(rank/25))
+        rank_page = int(float(((rank - 1) / 25) + 1))
         table_string = "{}&page={}".format(table, rank_page)
         target_url = self._request_build(table=table_string)
         response = requests.get(target_url).content
@@ -73,7 +73,7 @@ class Rankings(OSRSBase):
         time.sleep(0.1)
 
         table = OSRSInfo().alt_index_inverse[target]
-        rank_page = int(float(rank / 25))
+        rank_page = int(float(((rank-1) / 25) + 1))
         category_string = "1&table={}&page={}".format(table, rank_page)
         target_url = self._request_build(category_type=category_string)
         response = requests.get(target_url).content
--- tests/test_rankings.py
+++ tests/test_rankings.py
@@ -10,3 +10,8 @@ def test_lookup_skill():
 def test_lookup_nonskill():
     rank = Rankings().get_rank_in_target('clue_scrolls_all', 1)
     assert rank.score != 0
+
+
+def test_lookup_high_rank():
+    rank = Rankings().get_rank_in_skill('herblore', 5000)
+    assert rank.rank == 5000
